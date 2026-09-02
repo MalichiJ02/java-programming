4d0f832e8ee399080ad555f0853ffb2d5781d588
Malichi Josephat <malichij02@gmail.com>
2026-09-02 02:38:45 +0200

    variables

--- cs350ClassTasks/helloJava.java
+++ cs350ClassTasks/helloJava.java
@@ -6,5 +6,20 @@ public class helloJava{
         System.out.println("iam studying computer and iam in university ");
         System.out.println("i am learning cs 350");*/
         
+//design a Java program named StudentProfile. It should store and display:
+//name ,id ,age ,gender,year
 
+/*int age = 20 ;
+String name = "malichi josephat ";
+String ID = "1234455";
+String gender ="male ";
+int year =3;
+System.out.println("name: "+name );
+System.out.println("ID :"+ID);
+System.out.println("age :"+age);
+System.out.println("gender :"+gender);  
+System.out.println("year :"+year);
+*/
+//
+int first 
     }}
