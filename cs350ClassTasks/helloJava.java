b9b76584133abcd84282f1dc0e60ee9968b60847
Malichi Josephat <malichij02@gmail.com>
2026-09-02 02:25:22 +0200

    displaycomand

---
+++ cs350ClassTasks/helloJava.java
@@ -0,0 +1,10 @@
+public class helloJava{
+    public static void main (String[] args ){
+        
+       /* System.out.println("hello java!");
+        System.out.println("my name is malichi josephat ");
+        System.out.println("iam studying computer and iam in university ");
+        System.out.println("i am learning cs 350");*/
+        
+
+    }}
